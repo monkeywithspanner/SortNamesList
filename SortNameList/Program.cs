6f021025528d0881d03da35f00c82e7cb11c3604
monkeywithspanner <76044598+monkeywithspanner@users.noreply.github.com>
2025-10-24 17:09:38 +0100

    Add project files.

---
+++ SortNameList/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Name_sorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Check for argument
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: name-sorter <input-file-path>");
+                return;
+            }
+
+            string inputFile = args[0];
+            string outputFile = "sorted-names-list.txt";
+
+            try
+            {
+                // Read names from file
+                var names = File.ReadAllLines(inputFile)
+                                .Where(line => !string.IsNullOrWhiteSpace(line))
+                                .ToList();
+
+                // Sort names
+                var sortedNames = NameSorter.SortNames(names);
+
+                // Print to console
+                foreach (var name in sortedNames)
+                    Console.WriteLine(name);
+
+                // Write to file
+                File.WriteAllLines(outputFile, sortedNames);
+
+                Console.WriteLine($"\nSorted names written to {outputFile}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+
+
+    //I could have moved this small class to its own file but as it's trivial and only used here, I'll leave it here.
+    public static class NameSorter
+    {
+        public static List<string> SortNames(List<string> names)
+        {
+            return names.OrderBy(n =>
+            {
+                var parts = n.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string lastName = parts.Last();
+                string givenNames = string.Join(" ", parts.Take(parts.Length - 1));
+                return (lastName, givenNames);
+            }).ToList();
+        }
+    }
+}
+
---
+++ UnitTests/UnitTests1.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using Xunit;
+
+namespace Name_sorter.Tests
+{
+
+    public class NameSorterTests
+    {
+        [Fact]
+        public void SortNames_ShouldSortByLastName_ThenGivenNames()
+        {
+            // Arrange
+            var unsorted = new List<string>
+            {
+                "Janet Parsons",
+                "Vaughn Lewis",
+                "Adonis Julius Archer",
+                "Shelby Nathan Yoder",
+                "Marin Alvarez",
+                "London Lindsey",
+                "Beau Tristan Bentley",
+                "Leo Gardner",
+                "Hunter Uriah Mathew Clarke",
+                "Mikayla Lopez",
+                "Frankie Conner Ritter"
+            };
+
+            var expected = new List<string>
+            {
+                "Marin Alvarez",
+                "Adonis Julius Archer",
+                "Beau Tristan Bentley",
+                "Hunter Uriah Mathew Clarke",
+                "Leo Gardner",
+                "Vaughn Lewis",
+                "London Lindsey",
+                "Mikayla Lopez",
+                "Janet Parsons",
+                "Frankie Conner Ritter",
+                "Shelby Nathan Yoder"
+            };
+            // Act
+
+            var sorted = NameSorter.SortNames(unsorted);
+
+            // Assert
+            Assert.Equal(expected, sorted);
+        }
+
+        [Fact]
+        public void SortNames_ShouldSortByLastName_ThenGivenNames1()
+        {
+            // Arrange
+            var unsorted = new List<string>
+            {
+                "Janet Parsons",
+                "Vaughn Lewis",
+                "Adonis Julius Archer",
+                "Shelby Nathan Yoder",
+                "Marin Alvarez",
+                "London Lindsey",
+                "Beau Tristan Bentley",
+                "Leo Gardner",
+                "Hunter Uriah Mathew Clarke",
+                "Mikayla Lopez",
+                "Frankie Conner Ritter",
+            };
+
+            var expected = new List<string>
+            {
+                "Marin Alvarez",
+                "Adonis Julius Archer",
+                "Beau Tristan Bentley",
+                "Hunter Uriah Mathew Clarke",
+                "Leo Gardner",
+                "Vaughn Lewis",
+                "London Lindsey",
+                "Mikayla Lopez",
+                "Janet Parsons",
+                "Frankie Conner Ritter",
+                "Shelby Nathan Yoder",
+            };
+            // Act
+
+            var sorted = NameSorter.SortNames(unsorted);
+
+            // Assert
+            Assert.Equal(expected, sorted);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
